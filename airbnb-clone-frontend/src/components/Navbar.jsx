afe413c1f267c112a046384bb41f3b58a8d15791
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-06-26 12:59:22 +0300

    merging

---
+++ airbnb-clone-frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#FF5A5F', color: 'white' }}>
+      <h2>AirBnB Clone</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
